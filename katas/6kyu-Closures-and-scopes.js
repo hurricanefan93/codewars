ce8c40e406843f4a79e4e51688dd563b2db339c1
Garret Morales <hurricanefan93@gmail.com>
2017-07-13 22:37:43 -0400

    katas

---
+++ katas/6kyu-Closures-and-scopes.js
@@ -0,0 +1,9 @@
+Extend the String prototype by a trim function, that returns the string with leading or trailing whitespaces removed.
+
+Examples can be found in the test fixture.
+
+My solution:
+
+String.prototype.trim = function() {
+  return this.replace(/^(\s+)/, "").replace(/(\s+)$/, "");
+};
