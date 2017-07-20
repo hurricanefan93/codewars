a6a10be9578c86787db70f8ea0429ea151adb038
Garret Morales <hurricanefan93@gmail.com>
2017-07-20 00:34:50 -0400

    katas

---
+++ katas/6kyu-None-shall-pass.js
@@ -0,0 +1,11 @@
+The black knight moves for no man.
+However, you are no mere man; you are Arthur, King of the Britons.
+You have no quarrel with good sir knight, but you must cross this bridge!
+
+If this description does not make sense to you: the kata is a reference to Monty Python And The Holy Grail.
+
+
+My solution:
+
+difficulty = 'medium';
+BlackKnight.letsYouPass = function () { return true; }
